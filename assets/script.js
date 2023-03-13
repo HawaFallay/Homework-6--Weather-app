f081f908a625e23c12929de4332313cbc8c1183b
HawaFallay <113000340+HawaFallay@users.noreply.github.com>
2023-03-12 19:27:14 -0700

    added queryURL to script

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,10 @@
 var APIKey = "b5f92c4a26ccf01b987d99428065836f";
+var city = docu
+var temp = 
+var wind = 
+var 
+
+
+
+
+var queryURL = "hhtp://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
